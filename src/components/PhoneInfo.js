3eeb1192cfce669f0fece72094f20703c8864b5d
Miroljub <miroljub902@gmail.com>
2019-06-05 10:17:45 +0200

    phone list add, update, remove

---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+class PhoneInfo extends Component {
+    static defaultProps = {
+        info: {
+            name: "name",
+            phone: "000-000-000",
+            id: 0
+        }
+    }
+    state = {
+        editing: false,
+        name: '',
+        phone: ''
+    }
+
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+    handleToggleEdit = () => {
+        const { editing } = this.state;
+        this.setState({ editing: !editing });
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { info, onUpdate } = this.props;
+
+        if (!prevState.editing && this.state.editing) {
+            this.setState({
+                name: info.name,
+                phone: info.phone
+            });
+        }
+        if (prevState.editing && !this.state.editing) {
+            onUpdate(info.id, {
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }
+    }
+
+    render() {
+        const style = {
+            border: '1px solid black',
+            padding: '8px',
+            margin: '8px'
+        };
+
+        const { editing } = this.state;
+
+        if (editing) { //update mode
+            return (
+                <div style={style}>
+                    <input placeholder="name" value={this.state.name} onChange={this.handleChange} name="name" />
+                    <input placeholder="phone number" value={this.state.phone} onChange={this.handleChange} name="phone" />
+                    <button onClick={this.handleToggleEdit}>Apply</button>
+                    <button onClick={this.handleRemove}>Remove</button>
+                </div>
+            )
+        }
+
+        // normal mode
+
+        const {
+            name, phone
+        } = this.props.info;
+
+        return (
+            <div style={style}>
+                <div><b>{ name }</b></div>
+                <div>{ phone }</div>
+                <button onClick={this.handleToggleEdit}>Update</button>
+                <button onClick={this.handleRemove}>Remove</button>
+            </div>
+        );
+    }
+}
+
+export default PhoneInfo;
